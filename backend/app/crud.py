a574fd03f42378a6117782dde33b94bf790625ab
GopikasriRamesh <srigopika270@gmail.com>
2026-04-20 11:34:12 +0530

    added initial crud operations

---
+++ backend/app/crud.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Session
+from . import models, schemas, auth
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_pw = auth.get_password_hash(user.password)
+    db_user = models.User(email=user.email, hashed_password=hashed_pw)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_tasks(db: Session, user_id: int, completed: bool = None, skip: int = 0, limit: int = 10):
+    query = db.query(models.Task).filter(models.Task.owner_id == user_id)
+    if completed is not None:
+        query = query.filter(models.Task.completed == completed)
+    return query.offset(skip).limit(limit).all()
